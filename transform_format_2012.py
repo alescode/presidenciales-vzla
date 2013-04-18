6c122067dd1e1e22acf689c7964368993e468eb3
Alejandro Machado <alemacgo@gmail.com>
2013-04-18 18:04:11 +0200

    Agrega script para convertir data de ESDATA 2012 a nuestro formato

---
+++ transform_format_2012.py
@@ -0,0 +1,27 @@
+import pdb
+import csv
+import sys
+campos = ["state_id", "county_id", "township_id", "voting_center_id", "voting_table_id", "cne_new_code", "table_number"]
+candids = ["chavez", "capriles", "chirino", "sequera", "reyes", "bolivar", "NULOS"]
+filename = sys.argv[1]
+fout = open('newformat.csv','w')
+with open(filename, 'rb') as csvfile:
+    dialect = csv.Sniffer().sniff(csvfile.read(10024))
+    csvfile.seek(0)
+    reader = csv.reader(csvfile, dialect)
+    cnt = 0
+    for item in reader:
+        if cnt==0:
+            for item in campos:
+                fout.write(item+',')
+            fout.write('candidato,votos\n')
+
+        elif cnt >0:
+            cs = ''
+            for campo in [0, 2, 4, 8, 10, 9]:
+                cs = cs + item[campo]+','
+            for candidato in range(11,18):
+                fout.write(cs+candids[candidato-11]+','+item[candidato]+'\n')
+        cnt = cnt + 1
+
+fout.close()
