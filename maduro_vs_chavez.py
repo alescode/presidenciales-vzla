e406ce8d91aceeba8cd98a951fd7cdc3f5462f81
Alejandro Machado <alemacgo@gmail.com>
2013-04-26 02:29:54 +0200

    Parametriza opciones

--- maduro_vs_chavez.py
+++ maduro_vs_chavez.py
@@ -3,6 +3,16 @@
 
 import csv
 from collections import defaultdict
+from sys import argv, exit
+
+if len(argv) != 3:
+    print 'uso: maduro_vs_chavez DIFERENCIA_CON_CHAVEZ DIFERENCIA_PARTICIPACION'
+    print '\nej. DIFERENCIA_CON_CHAVEZ = 10 reporta las mesas en donde Maduro obtuvo 10% o mas de votos que Chavez.'
+    print 'ej. DIFERENCIA_PARTICIPACION = 300 filtra todas aquellas mesas donde la diferencia de participacion en ambas elecciones haya sido mayor a 300% (tres veces la cantidad de electores).'
+    exit(1)
+
+DIF_CON_CHAVEZ = int(argv[1])
+DIF_PARTICIPACION = int(argv[2])
 
 class Resultados(object):
     def __init__(self):
@@ -52,6 +62,19 @@ with open('raw/data_2013_raw.csv', 'rb') as csvfile:
             mesas[id_mesa].url13 = fila[22]
             mesas[id_mesa].url12 = fila[22].replace('2013', '2012')
 
+votos_chavez, votos_maduro, mesas_afectadas = 0, 0, 0
 for id_mesa, resultados in mesas.items():
-    if resultados.maduro > resultados.chavez and resultados.chavez != -1:
+    total12 = resultados.chavez + resultados.capriles12
+    total13 = resultados.maduro + resultados.capriles13
+    distancia = float(max(total12, total13)) / max(min(total12, total13), 1)
+    if (resultados.chavez != -1 and
+       resultados.maduro > (1 + (DIF_CON_CHAVEZ/100.0)) * resultados.chavez and
+       distancia < (DIF_PARTICIPACION/100.0)):
+        votos_chavez += resultados.chavez
+        votos_maduro += resultados.maduro
+        mesas_afectadas += 1
         print id_mesa, resultados, resultados.url13
+print ('\nHubo %d mesas donde Maduro tuvo %d%% mas votos que Chavez, solo contando centros en donde los votos por ambas opciones variaron en menos del %d%%.'
+% (mesas_afectadas, DIF_CON_CHAVEZ, DIF_PARTICIPACION))
+print ('\nEn estas mesas hubo %d votos para Maduro en 2013 y %d votos para Chavez en 2012, diferencia: %d'
+% (votos_maduro, votos_chavez, votos_maduro - votos_chavez))
