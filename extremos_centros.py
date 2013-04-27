c91387e264f379c1371f92b4fa9e496111e35765
Alejandro Machado <alemacgo@gmail.com>
2013-04-27 19:27:35 +0200

    Agrega extremos_centros

--- extremos_centros.py
+++ extremos_centros.py
@@ -6,7 +6,7 @@ from sys import argv, exit
 
 if len(argv) != 3 or (argv[2] != 'capriles' and argv[2] != 'maduro'):
     print 'uso: extremos PORCENTAJE CANDIDATO'
-    print '\nej. PORCENTAJE = 90, CANDIDATO=maduro reporta las mesas en donde Maduro obtuvo 90% o mas de los votos.'
+    print '\nej. PORCENTAJE = 90, CANDIDATO=maduro reporta los centros en donde Maduro obtuvo 90% o mas de los votos.'
     exit(1)
 
 PORCENTAJE = int(argv[1])
@@ -14,18 +14,18 @@ CANDIDATO = argv[2]
 
 class Resultados(object):
     def __init__(self):
-        self.maduro = -1
-        self.capriles = -1
+        self.maduro = 0
+        self.capriles = 0
         self.url13 = 'about:blank'
 
     def __str__(self):
         return ' Maduro: ' + str(self.maduro) +\
                ' Capriles: ' + str(self.capriles)
 
-mesas = {}
-# cada clave de este diccionario es una mesa, representada por el cne_new_id
-# del centro electoral, un guión, y el número de mesa en el centro.
-# cada una de las mesas en este diccionario está asociada a un objeto de tipo
+centros = {}
+# cada clave de este diccionario es una centro, representada por el cne_new_id
+# del centro electoral
+# cada uno de los centros en este diccionario está asociada a un objeto de tipo
 # Resultados.
 
 with open('raw/data_2013_raw.csv', 'rb') as csvfile:
@@ -35,15 +35,15 @@ with open('raw/data_2013_raw.csv', 'rb') as csvfile:
         if header is None:
             header = fila
         else:
-            id_mesa = fila[6] + '-' + fila[7]
-            if not mesas.has_key(id_mesa):
-                mesas[id_mesa] = Resultados()
-            mesas[id_mesa].maduro = int(fila[8])
-            mesas[id_mesa].capriles = int(fila[9])
-            mesas[id_mesa].url13 = fila[22]
+            id_centro = fila[6]
+            if not centros.has_key(id_centro):
+                centros[id_centro] = Resultados()
+                centros[id_centro].url13 = fila[22]
+            centros[id_centro].maduro += int(fila[8])
+            centros[id_centro].capriles += int(fila[9])
 
-votos_maduro, votos_capriles, mesas_afectadas = 0, 0, 0
-for id_mesa, resultados in mesas.items():
+votos_maduro, votos_capriles, centros_afectadas = 0, 0, 0
+for id_centro, resultados in centros.items():
     ganador = max(resultados.maduro, resultados.capriles)
     es_ganador = False
     if CANDIDATO == 'maduro' and ganador == resultados.maduro:
@@ -54,9 +54,9 @@ for id_mesa, resultados in mesas.items():
     if es_ganador and ganador > (PORCENTAJE/100.0) * total:
             votos_maduro += resultados.maduro
             votos_capriles += resultados.capriles
-            mesas_afectadas += 1
-            print id_mesa, resultados, resultados.url13
-print ('\nHubo %d mesas donde %s tuvo al menos el %d%% de los votos.'
-% (mesas_afectadas, CANDIDATO.capitalize(), PORCENTAJE))
-print ('\nEn estas mesas hubo %d votos para Maduro y %d votos para Capriles.'
+            centros_afectadas += 1
+            print id_centro, resultados, resultados.url13
+print ('\nHubo %d centros donde %s tuvo al menos el %d%% de los votos.'
+% (centros_afectadas, CANDIDATO.capitalize(), PORCENTAJE))
+print ('\nEn estas centros hubo %d votos para Maduro y %d votos para Capriles.'
 % (votos_maduro, votos_capriles))
